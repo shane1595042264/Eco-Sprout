11263642edc7e37a374aa8107ff3f471432f36e0
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2023-10-28 13:26:43 -0400

    stages

--- src/crops/Potato.js
+++ src/crops/Potato.js
@@ -3,6 +3,9 @@ const Potato = {
     harvestYield: 4,
     growthTime: 3,
     waterAbsorption: 1,
+    stage_1: "🌱",
+    stage_2: "🌱",
+    stage_3: "🌱",
   };
   
   export default Potato;
--- src/crops/Wheat.js
+++ src/crops/Wheat.js
@@ -3,6 +3,9 @@ const Wheat = {
     harvestYield: 3,
     growthTime: 2, // Time (in seconds) for each growth stage
     waterAbsorption: 2, // Affects nearby plants' growth
+    stage_1: "🌱",
+    stage_2: "🌱",
+    stage_3: "🌱",
   };
   
   export default Wheat;
